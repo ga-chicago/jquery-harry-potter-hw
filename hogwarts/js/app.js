bc0bb23066af1a103c07fe40b3a17934d90ffd52
Chris Roman <Christopherroman85@gmail.com>
2018-09-20 23:33:08 -0500

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -2,16 +2,25 @@ console.log('It is linked');
 
 const $container = $("#container");
 
-const $h1 = $("<h1>Hogwarts</h1>").append("#container");
+const $h1 = $("<h1>Hogwarts</h1>")
 
 const $h2 = $("<h2>Chris</h2>");
 
 const $h3 = $("<h3>Slytherin</h3>");
 
 const $petH4 = $("<h4 class='toad'>Citizen Cane</h4>")
+
 const $h4 = $("<h4>Birch Wand with a Phoenix Feather Core</h4>");
 
 const $ul = $("<ul> <li>Butter Beer</li><li class='secret'>Marauder's Map</li> <li class='secret'>Invisibility Cloak</li> <li>Time Turner</li> <li class='toad'>leash</li><li>Bertie Bott's Every Flavor Beans</li></ul>")
+
+const $h5 = $("<h5>Spring 2017</h5>")
+
+const $table1 = $("<table> <tr><th>Day</th><th>Classes</th></tr> <tr><th>Monday</th><th>Transfiguration, Charms</th></tr> <tr><th>Tuesday</th><th> Divination, History of Magic</th></tr> <tr><th>Wednesday</th><th>Transfiguration, Charms</th></tr></tr> <tr><th>Thursday</th><th>Herbology, Potions</th></tr> <tr><th>Friday</th><th>Defense Against the Dark Arts</th></tr> </table>")
+
+
+
+
 
 // This doesnt change the html console...
 
@@ -21,8 +30,9 @@ $container.append($h3)
 $container.append($petH4)
 $container.append($h4)
 $container.append($ul)
+$container.append($h5)
+$container.append($table1)
 
-$h1.text("Hogwarts")
 
 // This code doesn't change the html console either...
 
