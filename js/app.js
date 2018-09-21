9423dfebe88a66169af9c24cdb55c25494445bc7
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-20 19:17:35 -0500

    added more elements. My power grows with my dedication and hard work

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log('hogwarts')
 
+//----------------YEAR 1
+
 // Query for your div with the id of container and set it to a variable named $container
 const $container = $('#container');
 // console.log $container
@@ -8,8 +10,32 @@ console.log($container)
 const $h1 = $('<h1/>')
 console.log($h1);
 $('#container').append('<h1>Hogwarts</h1>')
-$($h1).text('<h1>Hogwarts</h1>')
 console.log($h1)
-
 // Add some text inside the h1 element. Example text: 'Hogwarts'
-// Why isn't your $h1 it appearing on your page?
+const $h2 = $('<h2/>')
+console.log($h2)
+//h2 element with your name
+$('#container').append('<h2>Josh Hepworth</h2>')
+console.log($h2)
+//h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
+const $h3 = $('<h3/>')
+console.log($h3)
+$('#container').append('<h3>Ravenclaw</h3>')
+// h4 element with your pet's name
+const $h4 = $('<h4/>')
+$('#container').append('<h4>Theodore</h4>')
+//this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+$('h4').addClass('dog')
+console.log($h4)
+// h4 element with your wand
+const $h4wand = $('<h4/>')
+$('#container').append('<h4>Elder Wand</h4>')
+
+
+
+
+
+
+
+
+
