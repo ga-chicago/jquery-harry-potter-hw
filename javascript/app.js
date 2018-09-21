6591b5a16237a12b45d4fcc1d43ce6904d1506aa
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-21 14:33:27 -0500

    Added more elements. My power grows with my dedication and hard work.

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,24 @@
 console.log('welcome to hogwarts');
 
-const $container = $('.container')
+const $container = $('.container');
 console.log($container);
 
-$('body').append('<h1/>').text("YOU'RE A WIZARD, ANDY");
+const $heading = $('<h1/>').text("YOU'RE A WIZARD, ANDY");
+$container.prepend($heading);
+
+
+const $name = $('<h2/>').text('Andy the Wizard');
+
+$container.append($name);
+
+const $house = $('<h3/>').text("House Ravenclaw");
+
+$container.append($house);
+
+const $pet = $('<h4 class = "owl" />').text("Kevin the Owl");
+
+$container.append($pet);
+
+const $wand = $('<h4/>').text("Holly Wand with Unicorn Hair Core");
+
+$container.append($wand);
