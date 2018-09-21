c0e0d06cab6ae56e4896458be17b45ba01dc8e7a
LehmanBrother <chrislehman1991@gmail.com>
2018-09-20 23:29:47 -0500

    Added more elements. My power grows with my dedication and hard work

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -1,7 +1,13 @@
 //Year 1
 const $container = $('#container');
-console.log($container);
+//console.log($container);
 $container.prepend('<h1></h1>');
 const $h1 = $('h1');
-console.log($h1);
+//console.log($h1);
 $h1.text('Hogwarts');
+
+//Year 2
+$container.append('<h2>Chris</h2>');
+$container.append('<h3>Ravenclaw</h2>');
+$container.append('<h4 class="snake">Colonel<h4>');
+$container.append('<h4>Prometheus<h4>');
