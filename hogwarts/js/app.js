54a0ea2af6e4b57d05bf768bcdcf03f88e148310
Chris Roman <Christopherroman85@gmail.com>
2018-09-21 00:06:41 -0500

    The days go by slowly and the years go by too fast

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -10,17 +10,25 @@ const $h3 = $("<h3>Slytherin</h3>");
 
 const $petH4 = $("<h4 class='toad'>Citizen Cane</h4>")
 
-const $h4 = $("<h4>Birch Wand with a Phoenix Feather Core</h4>");
+const $h4 = $("<h4>Red Mahogany Wand with Dragon Heartstring Core</h4>");
 
-const $ul = $("<ul> <li>Butter Beer</li><li class='secret'>Marauder's Map</li> <li class='secret'>Invisibility Cloak</li> <li>Time Turner</li> <li class='toad'>leash</li><li>Bertie Bott's Every Flavor Beans</li></ul>")
+// const $ul = $("<ul> <li>Butter Beer</li><li class='secret'>Marauder's Map</li> <li class='secret'>Invisibility Cloak</li> <li class ='secret'>Time Turner</li> <li class='toad'>leash</li><li>Bertie Bott's Every Flavor Beans</li></ul>")
 
 const $h5 = $("<h5>Spring 2017</h5>")
 
-const $table1 = $("<table> <tr><th>Day</th><th>Classes</th></tr> <tr><th>Monday</th><th>Transfiguration, Charms</th></tr> <tr><th>Tuesday</th><th> Divination, History of Magic</th></tr> <tr><th>Wednesday</th><th>Transfiguration, Charms</th></tr></tr> <tr><th>Thursday</th><th>Herbology, Potions</th></tr> <tr><th>Friday</th><th>Defense Against the Dark Arts</th></tr> </table>")
+const $table = $("<table> <tr><th>Day</th><th>Classes</th></tr> <tr><th>Monday</th><th>Transfiguration, Charms</th></tr> <tr><th>Tuesday</th><th> Divination, History of Magic</th></tr> <tr><th>Wednesday</th><th>Transfiguration, Charms</th></tr></tr> <tr><th>Thursday</th><th>Herbology, Potions</th></tr> <tr><th>Friday</th><th>Defense Against the Dark Arts</th></tr> </table>")
 
+const $li1 = $("<li>Butter Beer</li>")
 
+const $li2 = $("<li class='secret'>Marauder's Map</li>")
 
+const $li3 = $("<li class='secret'>Invisibility Cloak</li>")
 
+const $li4 = $("<li class ='secret'>Time Turner</li>")
+
+const $li5 = $("<li class='toad'>leash</li>")
+
+const $li6 = $("<li>Bertie Bott's Every Flavor Beans</li>")
 
 // This doesnt change the html console...
 
@@ -29,9 +37,14 @@ $container.append($h2)
 $container.append($h3)
 $container.append($petH4)
 $container.append($h4)
-$container.append($ul)
+$container.append($li1)
+$container.append($li2)
+$container.append($li3)
+$container.append($li4)
+$container.append($li5)
+$container.append($li6)
 $container.append($h5)
-$container.append($table1)
+$container.append($table)
 
 
 // This code doesn't change the html console either...
@@ -45,6 +58,14 @@ $container.append($table1)
 // $petH4.text("Citizen Cane")
 
 // $h4.text("Birch Wand with a Phoenix Feather Core")
+
+// $h4.remove()
+
+$li1.remove()
+
+$h4.css("color", "red")
+
+// $("head").append($petH4)
 
 
 
