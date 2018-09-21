b9201c97034ca94fefb25c0e36d5cf98424dc757
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-20 19:46:09 -0500

    Added list elements. That code didn't seem very DRY. I wonder if Hungry for More will have me DRY it up a bit.

--- js/app.js
+++ js/app.js
@@ -18,4 +18,20 @@ $('h4').addClass("cat")
 // console.log($h4);
 const $h4wand = $("<h4/>")
 $('#container').append("<h4>Awesome Wand</h4>")
+//--------  Year 3
 
+const $ul = $('<ul/>')
+$("#container").append("<ul></ul>")
+$('#ul').attr('storage')
+$('#ul').val('trunk')
+// console.log($ul);
+// $('#ul').append("<li>butterbeer</li>")
+
+const $li = $('<li/>')
+$('ul').append("<li>butterbeer</li>")
+$('ul').append("<li>invisibility</li>").addClass('secret')
+$('ul').append("<li>magic map</li>").addClass('secret')
+$('ul').append("<li>time turner</li>").addClass('secret')
+$('ul').append("<li>leash</li>").addClass('cat')
+$('ul').apped("<li>Bertie Bott's Every Flavor[Jelly] Beans </li>")
+$('container').append("ul")
