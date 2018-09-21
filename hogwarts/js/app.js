c0c4f30fd8c30771ff9b950addc8ac7f0c2b6881
LehmanBrother <chrislehman1991@gmail.com>
2018-09-20 23:35:47 -0500

    Added list elements. That code didn't seem very DRY. I wonder if Hungry for More will have me DRY it up a bit

--- hogwarts/js/app.js
+++ hogwarts/js/app.js
@@ -11,3 +11,12 @@ $container.append('<h2>Chris</h2>');
 $container.append('<h3>Ravenclaw</h2>');
 $container.append('<h4 class="snake">Colonel<h4>');
 $container.append('<h4>Prometheus<h4>');
+
+//Year 3
+$container.append('<ul storage="trunk"></ul>')
+$('ul').append('<li>butter beer</li>');
+$('ul').append('<li class="secret">invisibility cloak</li>');
+$('ul').append('<li class="secret">magic map</li>');
+$('ul').append('<li class="secret">time turner</li>');
+$('ul').append('<li class="snake">leash</li>');
+$('ul').append('<li>Bertie Bott&apos;s Every Flavor Beans</li>');
