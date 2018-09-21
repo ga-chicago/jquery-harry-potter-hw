0effec9e0d2757460bc48336a69de10c336da9a1
Charlotte Prevost <chaprevost91@gmail.com>
2018-09-20 21:59:26 -0500

    I added my first element and it was fun

--- hogwarts/javascript/app.js
+++ hogwarts/javascript/app.js
@@ -1,1 +1,19 @@
 console.log("JS IS RUNNING");
+
+// Declare variable, assign it the value of existing JQuery object
+const $container = $('.container');
+
+// console.log($container);			// ==> TEST
+
+// Declare variable, assign it value of now created JQuery object
+const $h1 = $('<h1/>');
+
+// console.log($h1);				// ==> TEST
+
+// Add text to object
+$h1.text('Hogwarts');
+
+// To $container append $h1
+$container.append($h1);
+
+// console.log($h1);				// ==> TEST
