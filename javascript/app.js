bc55206843e3381e3a8a26ddaca9c0053720d9b8
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-21 16:00:03 -0500

    The days go by slowly and the years go by too fast

--- javascript/app.js
+++ javascript/app.js
@@ -15,7 +15,7 @@ const $house = $('<h3/>').text("House Ravenclaw");
 
 $container.append($house);
 
-const $pet = $('<h4 class = "owl" />').text("Kevin the Owl");
+const $pet = $('<h4 class="owl" />').text("Kevin the Owl");
 
 $container.append($pet);
 
@@ -41,6 +41,17 @@ $container.append($('<h5>Spring 2017</h5>'));
 $container.append($('<table><thead><th>Day</th><th>Classes</th></thead><tr><td>Monday</td><td>Quidditch Practice</td></tr><tr><td>Tuesday</td><td>Charms, Potions</td></tr><tr><td>Wednesday</td><td>Herbology</td></tr><tr><td>Thursday</td><td>Defense against the dark arts</td></tr><tr><td>Tuesday</td><td>Transfiguration</td></tr></table>'));
 
 
-$wand.remove();
+$('h4').eq(1).remove();
 $('li').eq(0).remove();
+
+$('h4.owl').after('<h4>Bejeweled Dementor Bone Wand</h4>');
+// use remove and append again or just use appendTo method.....
+// decided on appendTo though understand this could be done in a couple of different ways
+$('h4.owl').appendTo($storageList);
+
+$('h4.owl').appendTo('h3');
+
+
+
+
 
