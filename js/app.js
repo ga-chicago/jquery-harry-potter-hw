b4b3d45057b593131f7e01fbe6a25c25806574ac
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-20 19:18:02 -0500

    Added more elements. My power grows with my dedication and hard work.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 console.log("works");
 
 const $container = $("#container");
-console.log($container);
+// console.log($container);
 
 const $h1 = $("<h1/>");
 $('#container').append('<h1>Hogwarts</h1>')
-console.log($h1);
+// console.log($h1);
+const $h2 = $("<h2/>");
+// console.log($h2);
+$('#container').append('<h2>Fil</h1>')
+const $h3 = $("<h3/>");
+// console.log($h3);
+$('#container').append('<h3>Slytherin</h3>')
+const $h4 = $("<h4/>");
+$('#container').append("<h4>Mac</h4>")
+$('h4').addClass("cat")
+// console.log($h4);
+const $h4wand = $("<h4/>")
+$('#container').append("<h4>Awesome Wand</h4>")
+
